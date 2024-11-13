8ea46b4b666e8c4556b99e164b3306b81169493c
RouaBenSlama <e2273144@cmaisonneuve.qc.ca>
2024-11-13 13:21:36 -0500

    installation firebase

--- app/src/main/java/com/example/piggybank_projet3/MainActivity.kt
+++ app/src/main/java/com/example/piggybank_projet3/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.piggybank_projet3
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialiser FirebaseAuth
+        auth = FirebaseAuth.getInstance()
+
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            // Si l'utilisateur n'est pas connecté, rediriger vers la page de connexion
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
